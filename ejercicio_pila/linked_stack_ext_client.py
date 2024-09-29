5b1cf793ef5fb60e7ad549f0299ec39741ec14d1
exe ansaldi <exeansaldi0@gmail.com>
2024-09-29 17:55:38 -0300

    debug metodos de deque

--- ejercicio_pila/linked_stack_ext_client.py
+++ ejercicio_pila/linked_stack_ext_client.py
@@ -9,6 +9,8 @@ class Linked_Stack_Ext_Client(LinkedStackExt):
     pila_nueva.push(5)
     pila_nueva.push(6)
 
-    print(pila_nueva.multi_pop(3))
+    print("Elementos eliminados de la pila ", pila_nueva.multi_pop(3))
+    pila_nueva.replace_all(2,6)
+    print("Pila luego de 3 pops y cambiando 2 por 6:\n",pila_nueva,"\n")
     
     
