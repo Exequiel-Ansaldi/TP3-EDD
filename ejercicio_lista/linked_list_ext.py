6a09065d6556b524cea8d7804a35f61be7bcbb99
exe ansaldi <exeansaldi0@gmail.com>
2024-09-28 21:29:43 -0300

    debug importaciones, falta debuggear metodos

--- ejercicio_lista/linked_list_ext.py
+++ ejercicio_lista/linked_list_ext.py
@@ -1,6 +1,6 @@
 from linked_list import LinkedList
 from linked_list_ext_abstract import LinkedListExtAbstract
-from Punto_1_y_2.linked_stack import LinkedStack
+from linked_stack import LinkedStack
 from list_node import ListNode
 from typing import Any, List
 
@@ -22,15 +22,15 @@ class LinkedListExt(LinkedList, LinkedListExtAbstract):
         if self._header.next is None:
             self.header = None
             return
-        current = self._header
-        while current.next.next is not None:
-            current = current.next
-        current.next = None
+        actual = self._header
+        while actual.next.next is not None:
+            actual = actual.next
+        actual.next = None
 
     def add_first(self, other: Any) -> None:
         new_node = ListNode(other)  
-        new_node.next = self.head
-        self.head = new_node
+        new_node.next = self._header
+        self._header = new_node
 
     def __iadd__(self, other: List[Any]) -> None:
         for elementos in reversed(other): 
